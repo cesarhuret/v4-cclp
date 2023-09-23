0ea6df13ad3aa7dc4dd28acf4d0048cbe06606c0
Cesar Huret <cesar.huret@gmail.com>
2023-09-23 10:20:28 -0700

    basic frontend

---
+++ my-app/src/App.tsx
@@ -0,0 +1,97 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+  Heading,
+  Input,
+  HStack,
+  Button,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Select,
+  Flex,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
+import { FaChevronDown } from "react-icons/fa"
+
+declare global {
+  interface Window{
+    ethereum?: any
+  }
+}
+const chains = [
+  ['Chain A', "http://18.196.63.236:8545/"],
+  ['Chain B', "http://3.79.184.123:8545/"]
+]
+
+export const App = () => {
+
+  const [lowerTick, setLowerTick] = React.useState(0)
+  const [upperTick, setUpperTick] = React.useState(0)
+  const [amount, setAmount] = React.useState(0)
+
+  const [account, setAccount] = React.useState('')
+  const [rpc, setRPC] = React.useState('')
+
+
+  const connect = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+
+    console.log(accounts)
+
+    setAccount(accounts[0])
+  }
+
+  const addLiquidity = () => {
+
+    console.log(lowerTick, upperTick, amount, rpc)
+
+  }
+
+  return (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <Flex gap={5} justifySelf="flex-end">
+          <Select maxW={'150px'} onChange={(e) => setRPC(e.target.value)}>
+            <option value={"http://18.196.63.236:8545/"}>Chain A</option>
+            <option value={"http://3.79.184.123:8545/"}>Chain B</option>
+          </Select>
+          {!account ? <Button w={'250px'} variant={'outline'} onClick={connect}>Connect Wallet</Button> : <span>{account.substring(0, 5)}...{account.substring(account.length - 5)}</span>}
+          <ColorModeSwitcher  />
+        </Flex>
+        <VStack spacing={8}>
+          <Heading>
+            Add Liquidity
+          </Heading>
+          <Box borderRadius={'xl'} maxW={'400px'} borderWidth={'0.1em'} p={10}>
+            <HStack spacing={5} pb={10}>
+              <Input size={'lg'} type='number' placeholder="Lower Tick"
+                onChange={(e) => {setLowerTick(parseFloat(e.target.value))}}
+              />
+              <Input size={'lg'} type='number' placeholder="Upper Tick"
+                onChange={(e) => {setUpperTick(parseFloat(e.target.value))}}
+              />
+            </HStack>
+            <Input size={'lg'} type='number' mb={10} placeholder="Amount" 
+              onChange={(e) => {setAmount(parseFloat(e.target.value))}}
+            />
+            <Button size={'lg'} w={'full'} onClick={addLiquidity} variant={'outline'}>Add Liquidity</Button>
+          </Box>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
+  }
